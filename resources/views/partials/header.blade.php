d0a1629e412a99a6139335564578ae45a5edd48c
samuelebruni <samuele_bruni@hotmail.com>
2023-10-26 16:31:21 +0200

    add header in welcome.blade.php

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,66 @@
+@extends('layouts.app')
+
+@section('header-content')
+<header>
+
+    <navbar id="app_navbar">
+        <div class="small">
+            <div class="container h-100">
+                <div class="d-flex justify-content-end gap-5 text-white align-items-center fw-bold h-100">
+                    <div>DC POWER VISA</div>
+                    <div>
+                        <span>ADDITIONAL DC SITES</span>
+                        <span>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16">
+                            <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
+                            </svg>              
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+            <div class="container py-1">
+                <div class="d-flex align-items-center justify-content-between">
+                    <div>
+                        <img src="{{ Vite::asset('resources/img/dc-logo.png')}}" alt="">
+                    </div>
+                    <div>
+                        <ul class="list-unstyled d-flex gap-4 fw-bold">
+                            <li>CHARACTERS</li>
+                            <li>COMICS</li>
+                            <li>MOVIES</li>
+                            <li>TV</li>
+                            <li>GAMES</li>
+                            <li>COLLECTIBILES</li>
+                            <li>VIDEOS</li>
+                            <li>FANS</li>
+                            <li>NEWS</li>
+                            <li>
+                                SHOP
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16">
+                                <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
+                                </svg>
+                            </li>
+                            <li class="searching">
+                                <div>
+                                    <div class="d-flex gap-1">
+                                        <div>
+                                            Search
+                                        </div>
+                                        <div>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                                                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>   
+                                            </svg>
+                                        </div>
+                                    </div>
+                                    <div class="barred"></div>
+                                </div>
+                            </li>
+                        </ul>
+                </div>
+            </div>
+        </navbar>
+
+</header>
+
+@endsection
